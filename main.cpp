8db3eb16cad2c24cc1122b6b2d768ce9aaee9433
ak2572 <ak2572@njit.edu>
2021-08-24 11:18:52 -0400

    Created CPU struct with registers

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using Byte = std::uint8_t;
+using Word = std::uint16_t;
+
+struct CPU {
+    // Registers of the Sharp LR35902
+    //
+    // Note: considering putting registers into array instead of individual
+    //       names, using functions to change a particular register
+    Byte A; Byte F;
+    Byte B; Byte C;
+    Byte D; Byte E;
+    Byte H; Byte L;
+    Word PC;
+    Word SP;
+};
+
+int main() {
+    return 0;
+}
